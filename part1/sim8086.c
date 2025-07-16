6f491fa7a7f6a501b1210e9ab1532364b4c8dd54
Utshav Dhakal <udhakal970@gmail.com>
2025-07-16 18:06:23 +0545

    Refactored sim8086.c and util.h for better code.

--- part1/sim8086.c
+++ part1/sim8086.c
@@ -5,214 +5,114 @@
 
 typedef unsigned char byte;
 
-void read_byte(FILE* file, char* value) {
-	fread(value, sizeof(byte), 1, file);
-	
-	if (feof(file)) {
-		printf("; EOF!\n");
-		exit(-1);
-	}
+FILE* file;
 
-	if (ferror(file)) {
-		printf("File Read Error!\n");
-		exit(-1);
-	}
-
-}
-
-char* decode_register(byte reg, byte width) {
+char* DecodeRegister(byte reg, byte width) {
 	switch(reg) {
-		case 0b000:
-			return width == 0 ? "al" : "ax";
-		case 0b001:
-			return width == 0 ? "cl" : "cx";
-		case 0b010:
-			return width == 0 ? "dl" : "dx";
-		case 0b011:
-			return width == 0 ? "bl" : "bx";
-		case 0b100:
-			return width == 0 ? "ah" : "sp";
-		case 0b101:
-			return width == 0 ? "ch" : "bp";
-		case 0b110:
-			return width == 0 ? "dh" : "si";
-		case 0b111:
-			return width == 0 ? "bh" : "di";
+		case 0b000: return width == 0 ? "al" : "ax";
+		case 0b001: return width == 0 ? "cl" : "cx";
+		case 0b010: return width == 0 ? "dl" : "dx";
+		case 0b011: return width == 0 ? "bl" : "bx";
+		case 0b100: return width == 0 ? "ah" : "sp";
+		case 0b101: return width == 0 ? "ch" : "bp";
+		case 0b110: return width == 0 ? "dh" : "si";
+		case 0b111: return width == 0 ? "bh" : "di";
 	}
 }
 
-void disassemble(FILE* file) {
+char* DecodeMemory(byte REG_MEM, byte MOD) {
+	char* MEM = (char*) malloc(20);
+	char* MEM_WITH_DISP = (char*) malloc(20);
+
+	switch (REG_MEM) {
+		case 0b000: MEM = "bx + si"; break;
+		case 0b001: MEM = "bx + di"; break;
+		case 0b010: MEM = "bp + si"; break;
+		case 0b011: MEM = "bp + di"; break;
+		case 0b100: MEM = "si"; break;
+		case 0b101: MEM = "di"; break;
+		case 0b110:
+			if (MOD == 0b00) MEM = "DIRECT ADDRESS?";
+			else MEM = "bp"; break;
+		case 0b111: MEM = "bx"; break;
+	}
+
+	if (MOD == 0b00) return MEM;
+
+	else if (MOD == 0b01) {
+		char DISP = ReadByte();
+		
+		if (DISP == 0) return MEM;
+		else if (DISP > 0) sprintf(MEM_WITH_DISP, "%s + %d", MEM, DISP);
+
+		return MEM_WITH_DISP;
+	}
+
+	else if (MOD == 0b10) {
+		short DISP = ReadWord();
+
+		if (DISP == 0) return MEM;
+		else if (DISP > 0) sprintf(MEM_WITH_DISP, "%s + %hi", MEM, *((short*) &DISP));
+
+		return MEM_WITH_DISP;
+	}
+}
+
+char* DecodeRegisterMemory(byte MOD, byte REG_MEM, byte WIDTH) {
+	switch (MOD) {
+		case 0b00:
+		case 0b01:
+		case 0b10:
+			return DecodeMemory(REG_MEM, MOD);
+		case 0b11:
+			return DecodeRegister(REG_MEM, WIDTH);
+	}
+}
+
+void ParseCopyRegisterMemoryToFromRegister(byte first) {
+	byte second;
+	second = ReadByte();
+
+	byte WIDTH = first & 0b00000001;
+	byte MOD = (second >> 6) & 0b00000011;
+	byte DIR = (first >> 1) & 0b00000001;
+
+	char *REG = DecodeRegister((second >> 3) & 0b00000111, WIDTH);
+	char *REG_MEM = DecodeRegisterMemory(MOD, second & 0b00000111, WIDTH);
+
+	if (MOD == 0b11) {
+		if (DIR) printf("mov %s, %s\n", REG, REG_MEM);
+		else printf("mov %s, %s\n", REG_MEM, REG);
+	}
+
+	else {
+		if (DIR) printf("mov %s, [%s]\n", REG, REG_MEM);
+		else printf("mov [%s], %s\n", REG_MEM, REG);
+	}
+}
+
+void ParseCopyImmediateToRegister(byte first) {
+	byte WIDTH = (first & 0b00001000) >> 3;
+	char* REG = DecodeRegister(first & 0b00000111, WIDTH);
+
+	if (WIDTH == 0) printf("mov %s, %d\n", REG, ReadByte());
+	else if (WIDTH == 1) printf("mov %s, %hi\n", REG, ReadWord());
+}
+
+void Disassemble() {
 	byte first;
 
 	while(1) {
-		read_byte(file, &first);
+		first = ReadByte();
 
 		byte OPCODE = (first & 0b11110000) >> 4;
-
-		if (OPCODE == 0b1011) {
-			// Immediate to register
-
-			byte WIDTH = (first & 0b00001000) >> 3;
-			char* REG = decode_register(first & 0b00000111, WIDTH);
-
-			if (WIDTH == 0) {
-				char DATA;
-				read_byte(file, &DATA);
-
-				printf("mov %s, %d\n", REG, DATA);
-			}
-			
-			else if (WIDTH == 1) {
-				char DATA[2];
-
-				read_byte(file, &DATA[0]);
-				read_byte(file, &DATA[1]);
-
-				short VAL = *((short*) &DATA);
-				printf("mov %s, %hi\n", REG, VAL);
-			}
-
-			continue;
+		switch (OPCODE) {
+			case 0b1011: ParseCopyImmediateToRegister(first); continue;
 		}
 		
 		OPCODE = (first & 0b11111100) >> 2;
-
 		switch(OPCODE) {
-
-			// Copy from R to R
-			// Copy from R to M
-			// Copy from M to R
-
-			case 0b100010: {
-				byte second;
-				read_byte(file, &second);
-
-				byte MOD = second >> 6;
-				byte WIDTH = first & 0b00000001;
-				byte DIR = first >> 1 & 0b00000001;
-
-
-				// Register Mode (No Displacement)
-
-				if (MOD == 0b11) {
-					char* reg1 = decode_register((second >> 3) & 0b00000111, WIDTH);
-					char* reg2 = decode_register(second & 0b00000111, WIDTH);
-
-					if (DIR == 0)
-						printf("mov %s, %s\n", reg2, reg1);
-					else if (DIR == 1)
-						printf("mov %s, %s\n", reg1, reg2);
-				}
-
-				// Memory Mode (No Displacement)
-
-				else if (MOD == 0b00) {
-					char* REG = decode_register((second >> 3) & 0b00000111, WIDTH);
-					char* MEM = (char*) malloc(20);
-
-					switch (second & 0b00000111) {
-						case 0b000:
-							MEM = "bx + si";
-							break;
-						case 0b001:
-							MEM = "bx + di";
-							break;
-						case 0b010:
-							MEM = "bp + si";
-							break;
-						case 0b011:
-							MEM = "bp + di";
-							break;
-						case 0b100:
-							MEM = "si";
-							break;
-						case 0b101:
-							MEM = "di";
-							break;
-						case 0b110:
-							MEM = "DIRECT ADDRESS?";
-							break;
-						case 0b111:
-							MEM = "bx";
-							break;
-					}
-
-					if (DIR == 0)
-						printf("mov [%s], %s\n", MEM, REG);
-					else if (DIR == 1)
-						printf("mov %s, [%s]\n", REG, MEM);
-				}
-
-				// Memory Mode (8-bit Displacement)
-
-				else if (MOD == 0b001) {
-					char* REG = decode_register((second >> 3) & 0b00000111, WIDTH);
- 					char* MEM = (char*) malloc(20);
-
-					char DISP;
-
-
-					switch (second & 0b00000111) {
-						case 0b110:
-
-							// BP + D8
-
-							read_byte(file, &DISP);
-
-							if (DISP > 0) sprintf(MEM, "bp + %d", DISP);
-							else MEM = "bp";
-
-							break;
-
-						case 0b000:
-
-							// BX + SI + D8
-
-							read_byte(file, &DISP);
-
-							if (DISP > 0) sprintf(MEM, "bx + si + %d", DISP);
-							else MEM = "bx + si";
-
-							break;
-					}
-
-					if (DIR == 0)
-						printf("mov [%s], %s\n", MEM, REG);
-					else if (DIR == 1)
-						printf("mov %s, [%s]\n", REG, MEM);
-				}
-
-				// Memory Mode (16-bit Displacement)
-
-				else if (MOD == 0b010) {
-					char* REG = decode_register((second >> 3) & 0b00000111, WIDTH);
- 					char* MEM = (char*) malloc(20);
-
-					char DISP[2];
-
-					switch (second & 0b00000111) {
-						case 0b000:
-
-							// BX + SI + D16
-
-							read_byte(file, &DISP[0]);
-							read_byte(file, &DISP[1]);
-
-							short VAL = *((short*) &DISP);
-
-							if (DISP > 0) sprintf(MEM, "bx + si + %hi", *((short*) &DISP));
-							else MEM = "bx + si";
-
-							break;
-					}
-
-					if (DIR == 0)
-						printf("mov [%s], %s\n", MEM, REG);
-					else if (DIR == 1)
-						printf("mov %s, [%s]\n", REG, MEM);
-				}
-			}
-				
+			case 0b100010: ParseCopyRegisterMemoryToFromRegister(first); continue;;
 		}
 	}
 }
@@ -220,21 +120,19 @@ void disassemble(FILE* file) {
 int main(int argc, char* argv[]) {
 
 	if (argc != 2) {
-		printf("Bad Argument!\n");
-		exit(-1);
+		printf("Bad Argument!\n"); exit(-1);
 	}
 
 	printf("; %s\n", argv[1]);
 	printf("bits 16\n");
 
-	FILE* file = fopen(argv[1], "rb");
+	file = fopen(argv[1], "rb");
 
 	if (file == NULL) {
-		printf("File Error!\n");
-		exit(-1);
+		printf("File Error!\n"); exit(-1);
 	}
 
-	disassemble(file);
+	Disassemble();
 
 	fclose(file);
 	return 0;
--- part1/util.h
+++ part1/util.h
@@ -1,6 +1,6 @@
 typedef unsigned char byte;
 
-char* binary(byte b) {
+char* Binary(byte b) {
     char* result = malloc(9);
     result[8] = '\0';
     result[0] = (b & 0x80 ? '1' : '0');
@@ -12,4 +12,28 @@ char* binary(byte b) {
     result[6] = (b & 0x02 ? '1' : '0');
     result[7] = (b & 0x01 ? '1' : '0');
     return result;
+}
+
+char ReadByte(void) {
+	char Data;
+	fread(&Data, 1, 1, file);
+	
+	if (feof(file)) {
+		printf("; EOF!\n"); exit(-1);
+	}
+
+	if (ferror(file)) {
+		printf("File Read Error!\n"); exit(-1);
+	}
+
+	return Data;
+}
+
+short ReadWord() {
+	char Data[2];
+
+	Data[0] = ReadByte();
+	Data[1] = ReadByte();
+
+	return *((short*) &Data);
 }
